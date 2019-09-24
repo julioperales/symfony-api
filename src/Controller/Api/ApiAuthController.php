4eb6047e9ef080dc009533c4984734163163b36c
juldelos <julio.delosperales@prodigious.com>
2019-09-24 09:02:55 +0200

    till return token

---
+++ src/Controller/Api/ApiAuthController.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Controller\Api;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * @Route("/auth")
+ */
+class ApiAuthController extends AbstractController
+{
+    /**
+     * @Route("/register", name="api_auth_register",  methods={"POST", "GET"})
+     * @param Request $request
+     * @param UserManagerInterface $userManager
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function register(Request $request, UserManagerInterface $userManager)
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        /*
+        $data["username"] = $request->query->get("username");
+        $data["password"] = $request->query->get("password");
+        $data["email"] = $request->query->get("email");
+        */
+        
+        $validator = Validation::createValidator();
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'username' => new Assert\Length(array('min' => 1)),
+            'password' => new Assert\Length(array('min' => 1)),
+            'email' => new Assert\Email(),
+        ));
+        $violations = $validator->validate($data, $constraint);
+        if ($violations->count() > 0) {
+            return new JsonResponse(["error" => (string)$violations], 500);
+        }
+        $username = $data['username'];
+        $password = $data['password'];
+        $email = $data['email'];
+        $user = new User();
+        $user
+            ->setUsername($username)
+            ->setPlainPassword($password)
+            ->setEmail($email)
+            ->setEnabled(true)
+            ->setRoles(['ROLE_USER'])
+            ->setSuperAdmin(false)
+        ;
+        try {
+            $userManager->updateUser($user, true);
+        } catch (\Exception $e) {
+            return new JsonResponse(["error" => $e->getMessage()], 500);
+        }
+        return new JsonResponse(["success" => $user->getUsername(). " has been registered!"], 200);
+    }
+
+    /**
+     * @Route ("/test", name="test_api")
+     */
+    public function test(){        
+        return new JsonResponse(["test" => "test"]);
+    }
+}
